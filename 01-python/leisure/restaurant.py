dc68dd0a377276f62c6cb9a6c18cef6f25a1eb63
lingxiao yuan <islxyuan@gmail.com>
2021-02-07 15:29:19 -0500

    implementing restaurant, added a menu

--- 01-python/leisure/restaurant.py
+++ 01-python/leisure/restaurant.py
@@ -1,3 +1,4 @@
 def draw_restaurant():
-    print("restaurant not found")
+    print("This is a restaurant")
+    print("The menu of the restaurant: Chinese Food, Indian Food, American Food, Drinks")
     return
